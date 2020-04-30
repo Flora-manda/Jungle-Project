32e7c644fd65e89a74a05ec4d6a1c5f0aebd7097
Flora <flora.uwadiegwu@gmail.com>
2020-04-30 21:41:14 +0000

    update dashboard controller and view to show product stat

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,15 @@
 class Admin::DashboardController < ApplicationController
   def show
   end
+
+  def product_count
+    Product.count(:all)
+  end
+  helper_method :product_count
+
+  def category_count
+    Category.count(:all)
+  end
+  helper_method :category_count
+  
 end
