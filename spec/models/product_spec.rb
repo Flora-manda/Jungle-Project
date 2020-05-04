aca0fba4a27a53ca37c8b776db60b4bc9a0a4554
Flora <flora.uwadiegwu@gmail.com>
2020-05-04 18:53:50 +0000

    create test for Product model

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,42 @@
+require 'rails_helper'
+
+RSpec.describe Product, type: :model do
+  describe 'Validations' do
+    subject { 
+      described_class.new(
+        name: "Anything",
+        description: "Anything",
+        image: "apparel1.jpg",
+        price_cents: 100,
+        quantity: 100,
+        created_at: DateTime.now,
+        updated_at: DateTime.now + 1.week,
+        category: Category.new)
+      }
+    
+
+    it "is valid with valid attributes" do
+      expect(subject).to be_valid
+    end
+
+    it "is not valid without a name" do
+      subject.name = nil
+      expect(subject).to_not be_valid
+    end
+
+    it "is not valid without a price" do
+      subject.price_cents = nil
+      expect(subject).to_not be_valid
+    end
+
+    it "is not valid without a quantity" do
+      subject.quantity = nil
+      expect(subject).to_not be_valid
+    end
+
+    it "is not valid without a category" do
+      subject.category = nil
+      expect(subject).to_not be_valid
+    end
+  end
+end
